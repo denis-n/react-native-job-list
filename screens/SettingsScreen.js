29fc7d7d6a085a319ab8af69f69a698ada26290e
qi <12meridiansmedia@gmail.com>
2018-06-02 16:07:28 +0300

    Reset liked jobs.

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { Button, Card } from "react-native-elements";
+import { connect } from "react-redux";
+
+import * as actions from "../actions";
 
 class SettingsScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>Settings Screen</Text>
-      </View>
+      <Card title="Reset">
+        <Button
+          large
+          backgroundColor="#009688"
+          icon={{ name: "delete-forever" }}
+          title="Reset Liked Jobs"
+          onPress={this.props.clearLikedJobs}
+        />
+      </Card>
     );
   }
 }
 
-export default SettingsScreen;
+export default connect(null, actions)(SettingsScreen);
